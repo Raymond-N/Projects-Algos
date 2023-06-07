e1a2984d39b3dcef0e4b44f1f8221f1854936270
Raymond Natividad <natividad2315@gmail.com>
2023-06-07 00:28:39 -0700

    Continued challenges. Stuck on challenge 8.

--- chapters/algorithm_concepts_and_tips/optional_assignments/fundamentals_to_do_3.js
+++ chapters/algorithm_concepts_and_tips/optional_assignments/fundamentals_to_do_3.js
@@ -57,3 +57,69 @@ console.log(printOneReturnAnother([2, 4, 7, 10, 11]));
 
 
 // CHALLENGE 4
+function double(arr) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] * 2);
+    }
+    console.log(newArr);
+    return newArr;
+}
+double([1,2,3]);
+
+
+// CHALLENGE 5
+function countPositives(arr) {
+    count = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] >= 0) {
+            count++;
+        }
+    }
+    arr[arr.length - 1] = count;
+    console.log(arr);
+    return arr;
+}
+countPositives([-1,1,1,1]);
+
+
+// CHALLENGE 6
+function evensAndOdds(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0 && arr[i + 2] % 2 !== 0) {
+            console.log("That's odd!");
+        } else if(arr[i] % 2 === 0 && arr[i + 1] % 2 === 0 && arr[i + 2] % 2 === 0) {
+            console.log("Even more so!");
+        }
+    }
+}
+evensAndOdds([1, 3, 5, 2, 4, 6, 7, 8, 9]);
+evensAndOdds([2, 4, 6, 1, 3, 5, 8, 10, 12]);
+evensAndOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+
+// CHALLENGE 7
+function incrementTheSeconds(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if(i % 2 !== 0) {
+            arr[i] += 1;
+        }
+        console.log(arr[i]);
+    }
+    console.log(arr);
+    return arr;
+}
+incrementTheSeconds([1,3,5,7,9,11]);
+
+
+// CHALLENGE 8
+// keep working!
+function previousLengths(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        arr[i] = arr[i - 1].length;
+    }
+    return arr;
+}
+const array = ['apple', 'banana', 'orange', 'kiwi'];
+const modifiedArray = previousLengths(array);
+console.log(modifiedArray);
